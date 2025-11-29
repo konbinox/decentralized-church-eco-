0b10144cc48f718cd46a7881f9a20fa1df4928f5
konbinox <95138368+konbinox@users.noreply.github.com>
2025-11-29 16:30:07 -0500

    Delete modules/title-explosive.js

--- modules/title-explosive.js
+++
@@ -1,6 +0,0 @@
-export function renderTitleExplosive(data) {
-  const template = document.getElementById("title-explosive").innerHTML;
-  return template
-    .replace("{{text}}", data.text || "")
-    .replace("{{subtitle}}", data.subtitle || "");
-}
