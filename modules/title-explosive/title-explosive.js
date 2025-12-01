8b6f86b8cbf05f2cbb417cccef260a9c279e1b2a
konbinox <95138368+konbinox@users.noreply.github.com>
2025-12-01 14:26:43 -0500

    Update title-explosive.js

--- modules/title-explosive/title-explosive.js
+++ modules/title-explosive/title-explosive.js
@@ -1,8 +1,4 @@
 export function renderExplosive(data) {
   const template = document.getElementById("title-explosive").innerHTML;
   return template.replace("{{text}}", data.text || "");
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 4a68468905ce0622c2dfd4d057aa1a140c1b4ef2
